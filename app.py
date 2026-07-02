699e727d36a898586bcec284932becd630c72056
Srivaskumara <SRIVASKUMARA@GMAIL.COM>
2026-07-02 14:09:36 -0500

    remove vulerabillities

--- app.py
+++ app.py
@@ -1,18 +1,11 @@
 from flask import Flask , request
 import os 
 
-GITHUB_TOKEN = "ghp_12345678901234567890123455"
-
 app = Flask(__name__)
 
 @app.route("/")
 def home():
     return "Secure DevSecOps Pipeline is running!"
-
-@app.route("/run")
-def run_command():
-    cmd=request.args.get("cmd")
-    return os.popen(cmd).read()
 
 
 if __name__== "__main__":
